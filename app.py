95a40e49f2386fc7b7738f348f3ff5481f1fba63
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-01-24 15:58:38 -0800

    Sending and validating word between front and back end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -13,4 +13,22 @@ def load_game():
     high_score = session.get("high_score", 0)
     num_of_plays = session.get("num_of_plays", 0)
 
-    return render_template("index.html", board=board, high_score=high_score, num_of_plays=num_of_plays)
+    return render_template("index.html",
+                           board=board,
+                           high_score=high_score,
+                           num_of_plays=num_of_plays)
+
+
+@app.route("/submit", methods=["POST"])
+def validate_word():
+    #request.json because axios sends info with json
+    word = request.json["word"]
+    board = session["board"]
+    validity = boggle_game.check_valid_word(board, word)
+    
+    if validity == "ok":
+        return jsonify({"msg": f"Added:{word}", "cls": "success"})
+    elif validity == "not-word":
+        return jsonify({"msg": f"{word} is not a valid English word.", "cls": "fail"}) 
+    else: 
+        return jsonify({"msg": f"{word} is not a word on the board.", "cls": "fail"}) 
--- static/script.js
+++ static/script.js
@@ -1,13 +1,32 @@
-let played_words = Set();
+let played_words = new Set();
+
+$(function() {
+  $(".btn").on("click", function(event) {
+    handleClick(event);
+  })
+})
 
 async function handleClick(event) {
   event.preventDefault();
-  let word = $("#word").val();
+  let word = $("#word").val().toLowerCase();
 
   if (played_words.has(word)) {
     // this word has ben played before
   } else {
-    // post to our submit rout
+      // post to our submit route get back the status of the word
+    let response = (await axios.post('/submit', {"word": word})).data;
+    console.log(response);
+      // post message accordingly
+    // add word to ul if valid
+    // update score with length of word
+
   }
 
 }
+
+// function showMessage(msg, cls) {
+
+// }
+
+// function 
+
